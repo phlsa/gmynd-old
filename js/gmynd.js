add22d4eecc6035beb82f09048a7e14e4431de60
Philipp Sackl <philipp@envis-precisely.com>
2013-06-30 13:25:13 +0200

    fix nesting bug

--- js/gmynd.js
+++ js/gmynd.js
@@ -56,9 +56,7 @@ window.Gmynd = {
 			_.extend( this, params );
 		};
 		this.setParent = function( target ) {
-			// Seems better not to call remove(), so the event handlers that are already attached will not be lost
-			//this.elem.remove();
-			this.elem.appendTo( target.elem );
+			this.elem.appendTo( target );
 		}
 	},
 
